6fe3183694ee270f59eaa059ff4dea986b9778dd
Karl Johann Schubert <karljohann@familieschubi.de>
2016-05-31 03:21:48 +0200

    plugin active updated

--- plugins/active.php
+++ plugins/active.php
@@ -18,7 +18,7 @@ class active implements Plugin {
         // check if person has active positions
         if(count($current_person->current_positions) > 0) {
             // remove active:no
-            if(!$this->removeRole($current_person->person->id, "active", "no")) return false;
+            $this->removeRole($current_person->person->id, "active", "no");
 
             // search for latest end date
             $end_date = 0;
@@ -27,17 +27,14 @@ class active implements Plugin {
             }
 
             // add active:yes
-            if(!$this->addRole($current_person->person->id, "active", "yes", date('c', $end_date))) return false;
-
-            return true;
+            $this->addRole($current_person->person->id, "active", "yes", date('c', $end_date));
         } else {
             // if there are no positions remove active:yes and add active:no
-            if($this->removeRole($current_person->person->id, "active", "yes") && $this->addRole($current_person->person->id, "active", "no")) {
-                return true;
-            } else {
-                return false;
-            }
+            $this->removeRole($current_person->person->id, "active", "yes");
+            $this->addRole($current_person->person->id, "active", "no");
         }
+
+        return true;
     }
 
     function onFirstLogin($authProvider, $current_person)
@@ -70,77 +67,38 @@ class active implements Plugin {
     }
 
     private function getId($type, $name) {
-        $conn = DBController::getConnection();
-        if($conn) {
-            $table = "";
-            switch ($type) {
-                case 'scope':
-                    $table = '`scopes`';
-                    break;
+        $table = "";
+        switch ($type) {
+            case 'scope':
+                $table = '`scopes`';
+                break;
 
-                case 'role':
-                    $table = '`roles`';
-                    break;
+            case 'role':
+                $table = '`roles`';
+                break;
 
-                default:
-                    return false;
-            }
-            if ($result = $conn->query("SELECT `id` FROM " . $table . " WHERE `name`='" . $conn->real_escape_string($name) . "' LIMIT 1")) {
-                if ($result->num_rows == 1) {
-                    return $result->fetch_row()[0];
-                } else {
-                    if ($conn->insert("INSERT INTO " . $table . " SET `name`='" . $conn->real_escape_string($name) . "'") === TRUE) {
-                        return mysqli_insert_id($conn);
-                    } else {
-                        trigger_error("Database Error: " . $conn->error, E_USER_ERROR);
-                        return false;
-                    }
-                }
-            } else {
+            default:
                 return false;
-            }
+        }
+        $result = DBController::query("SELECT `id` FROM " . $table . " WHERE `name`='" . DBController::escape($name) . "' LIMIT 1");
+        if ($result->num_rows == 1) {
+            return $result->fetch_row()[0];
         } else {
-            return false;
+            return DBController::insert("INSERT INTO " . $table . " SET `name`='" . DBController::escape($name) . "'");
         }
     }
 
     private function addRole($personId, $scope, $role, $expires_at = null) {
-        $conn = DBController::getConnection();
-        if($conn) {
-            $scope = $this->getId('scope', $scope);
-            $role = $this->getId('role', $role);
-            if ($scope === false || $role === false) {
-                return false;
-            } else {
-                if ($conn->query("INSERT IGNORE INTO `persons_scopes` (`person_id`, `scope_id`, `role_id`, `expires_at`) VALUES (" . intval($personId) . ", " . intval($scope) . ", " . intval($role) . ", " . (($expires_at == null) ? "NULL" : "'" . $expires_at . "'") . ")")) {
-                    return true;
-                } else {
-                    trigger_error("Database Error: " . $conn->error, E_USER_ERROR);
-                    return false;
-                }
-            }
-        } else {
-            return false;
-        }
+        $scope = $this->getId('scope', $scope);
+        $role = $this->getId('role', $role);
+
+        DBController::insert("INSERT IGNORE INTO `persons_scopes` (`person_id`, `scope_id`, `role_id`, `expires_at`) VALUES (" . intval($personId) . ", " . intval($scope) . ", " . intval($role) . ", " . (($expires_at == null) ? "NULL" : "'" . $expires_at . "'") . ")");
     }
 
     private function removeRole($personId, $scope, $role) {
-        $conn = DBController::getConnection();
-        if($conn) {
-            $scope = $this->getId('scope', $scope);
-            $role = $this->getId('role', $role);
-            if ($scope === false || $role === false) {
-                return false;
-            } else {
-                if ($conn->query("DELETE FROM `persons_scopes` WHERE `person_id`=" . intval($personId) . " AND `scope_id`=" . intval($scope) . " AND `role_id`=" . intval($role) . " LIMIT 1")) {
-                    return true;
-                } else {
-                    trigger_error("Database Error: " . $conn->error, E_USER_ERROR);
-                    return false;
-                }
-            }
-        } else {
-            return false;
-        }
+        $scope = $this->getId('scope', $scope);
+        $role = $this->getId('role', $role);
+
+        DBController::delete("DELETE FROM `persons_scopes` WHERE `person_id`=" . intval($personId) . " AND `scope_id`=" . intval($scope) . " AND `role_id`=" . intval($role) . " LIMIT 1");
     }
 }
